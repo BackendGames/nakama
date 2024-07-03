04bba8fbce61f0200cf65f18ca70381e3ae664f4
Ahmet Kaplan <kaplan38@gmail.com>
2024-07-03 20:11:16 +0300

    fix definitions

--- apigrpc/frostline_build.go
+++ apigrpc/frostline_build.go
@@ -1,6 +1,3 @@
-//go:build tools
-// +build tools
-
 // Copyright 2020 The Nakama Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,11 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package build
+package apigrpc
 
-import (
-	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
-	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
-)
+//go:generate protoc -I. -I../vendor -I../build/grpc-gateway-v2.3.0/third_party/googleapis -I../vendor/github.com/grpc-ecosystem/grpc-gateway/v2 --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --grpc-gateway_out=. --grpc-gateway_opt=paths=source_relative --grpc-gateway_opt=logtostderr=true --openapiv2_out=. --openapiv2_opt=logtostderr=true,allow_delete_body=true apifrostline.proto
